e381bab1cd3200c592dfd8de48658b9b73908bb4
saraniya-automation <saraajaysaisha@gmail.com>
2025-07-03 11:57:36 +0530

    emergency_contact single

--- app/schemas/employee_profile_schema.py
+++ app/schemas/employee_profile_schema.py
@@ -15,7 +15,8 @@ from marshmallow import Schema, fields
 class EmployeeProfileSchema(Schema):
     personal_details = fields.Dict()
     contact_details = fields.Dict()
-    emergency_contacts = fields.List(fields.Dict())
+    # emergency_contacts = fields.List(fields.Dict())
+    emergency_contacts = fields.Dict()
     dependents = fields.List(fields.Dict())
     job_details = fields.Dict()
     salary_details = fields.Dict()
